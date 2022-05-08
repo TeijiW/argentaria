c8fef3c8898f2af501cf270c23f9a235fac8b14c
Alessandro Teiji <teijiwatanabe01@gmail.com>
2022-05-08 18:23:02 -0400

    Withdraw event and fallback action for events controller

--- lib/argentaria_web/controllers/events_controller.ex
+++ lib/argentaria_web/controllers/events_controller.ex
@@ -3,6 +3,8 @@ defmodule ArgentariaWeb.EventsController do
   alias Argentaria.Accounts
   alias Accounts.Account
 
+  action_fallback ArgentariaWeb.FallbackController
+
   def create(
         conn,
         %{"type" => "deposit", "destination" => account_id, "amount" => value} = _params
@@ -10,8 +12,23 @@ defmodule ArgentariaWeb.EventsController do
     account_id
     |> Accounts.get_one()
     |> save_account({account_id, value})
-    |> handle_save_response(conn, :deposit)
+    |> handle_response(conn, :deposit)
   end
+
+  def create(
+        conn,
+        %{"type" => "withdraw", "origin" => account_id, "amount" => value} = _params
+      ) do
+    account_id
+    |> Accounts.get_one()
+    |> subtract_from_account({account_id, value})
+    |> handle_response(conn, :withdraw)
+  end
+
+  defp subtract_from_account({:ok, account = %Account{}}, {_account_id, value}),
+    do: Accounts.update(account, %{balance: account.balance - value})
+
+  defp subtract_from_account({:error, :not_found}, _params), do: {:error, :not_found}
 
   defp save_account({:ok, account = %Account{}}, {_account_id, value}),
     do: Accounts.update(account, %{balance: account.balance + value})
@@ -19,13 +36,17 @@ defmodule ArgentariaWeb.EventsController do
   defp save_account({:error, :not_found}, {account_id, value}),
     do: Accounts.insert(%{id: account_id, balance: value})
 
-  defp handle_save_response({:ok, account}, conn, :deposit) do
+  defp handle_response({:ok, account}, conn, :deposit) do
     conn
     |> put_status(:created)
     |> render("deposit.json", account: account)
   end
 
-  defp handle_save_response({:error, changeset}, conn, _event_type) do
-    nil
+  defp handle_response({:ok, account}, conn, :withdraw) do
+    conn
+    |> put_status(:created)
+    |> render("withdraw.json", account: account)
   end
+
+  defp handle_response({:error, _reason} = error, _conn, _event_type), do: error
 end
--- lib/argentaria_web/views/events_view.ex
+++ lib/argentaria_web/views/events_view.ex
@@ -2,6 +2,12 @@ defmodule ArgentariaWeb.EventsView do
   use ArgentariaWeb, :view
 
   def render("deposit.json", %{account: account}) do
-    %{destination: %{id: account.id, balance: account.balance}}
+    %{destination: build_account_info(account)}
   end
+
+  def render("withdraw.json", %{account: account}) do
+    %{origin: build_account_info(account)}
+  end
+
+  defp build_account_info(account), do: %{id: account.id, balance: account.balance}
 end
