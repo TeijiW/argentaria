19d00b82f4ba5c0e3ed21a93e2426693fd91f26f
Alessandro Teiji <teijiwatanabe01@gmail.com>
2022-05-08 18:09:50 -0400

    Fallback controller and setup for balance controller

--- lib/argentaria_web/controllers/balance_controller.ex
+++ lib/argentaria_web/controllers/balance_controller.ex
@@ -2,15 +2,10 @@ defmodule ArgentariaWeb.BalanceController do
   use ArgentariaWeb, :controller
   alias Argentaria.Accounts
 
-  def show(conn, %{"account_id" => account_id} = _params) do
-    case Accounts.get_one(account_id) do
-      {:ok, account} ->
-        json(conn, account.balance)
+  action_fallback ArgentariaWeb.FallbackController
 
-      {:error, :not_found} ->
-        conn
-        |> put_status(:not_found)
-        |> json(0)
-    end
+  def show(conn, %{"account_id" => account_id} = _params) do
+    with {:ok, account = %Accounts.Account{}} <- Accounts.get_one(account_id),
+         do: json(conn, account.balance)
   end
 end
---
+++ lib/argentaria_web/controllers/fallback_controller.ex
@@ -0,0 +1,5 @@
+defmodule ArgentariaWeb.FallbackController do
+  use ArgentariaWeb, :controller
+
+  def call(conn, {:error, :not_found}), do: conn |> put_status(:not_found) |> json(0)
+end
